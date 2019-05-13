870d9a2d014b8c5e18c281700ed3523260147820
Karim B <mattiakarim@live.it>
2019-05-13 16:59:04 +0200

    Could not complete the setInterval stretch goal on time

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,1 +1,49 @@
 console.log('I am working');
+let count = 0;
+let intervalSeconds = window.setInterval(counterSecond, 1000);
+let intervalSingleDigitSeconds = window.setInterval(counterSingleDigitSeconds, 100);
+let intervalHundreds = window.setInterval(counterHundreds, 100);
+let intervalMilliSeconds = window.setInterval(counterMilliSeconds, 100);
+
+function counterSecond() { 
+  // Your code here
+  if(count < 1){
+    document.querySelector('#secondTens').textContent = ++count;    
+  } else if(count > 1){
+    count = 0;
+    document.querySelector('#secondTens').textContent = ++count;   
+  }   
+}
+
+function counterSingleDigitSeconds() { 
+    // Your code here
+    if(count < 9){
+        document.querySelector('#secondOnes').textContent = ++count;    
+    } else if(count >= 9){
+        count = 0;
+        document.querySelector('#secondOnes').textContent = ++count;   
+    }
+    
+}
+
+function counterHundreds() { 
+    // Your code here
+    if(count <= 9){
+        document.querySelector('#msHundreds').textContent = ++count;   
+    } else if(count > 9){
+        count = 0;
+        document.querySelector('#msHundreds').textContent = ++count;  
+    }
+}
+
+function counterMilliSeconds() { 
+    // Your code here
+    if(count <= 9){
+        document.querySelector('#msTens').textContent = ++count;    
+    } else if(count > 9){
+        count = 0;
+        document.querySelector('#msTens').textContent = ++count;   
+    }
+}
+
+
